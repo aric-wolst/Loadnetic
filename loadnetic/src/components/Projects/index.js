d3d5c967b08d249b9262e16a8deeda7f92812788
aric-wolst <aric.wolst@gmail.com>
2019-08-14 19:02:47 -0700

    projects page start

--- loadnetic/src/components/Projects/index.js
+++ loadnetic/src/components/Projects/index.js
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Switch, Route, Link } from 'react-router-dom';
+import { compose } from 'recompose';
+
+import { withFirebase } from '../Firebase';
+import { withAuthorization } from '../Session';
+import * as ROUTES from '../../constants/routes';
 
 const ProjectsPage = () => (
     <div>
-        Projects
+        <h1>Projects</h1>
     </div>
-)
+);
 
-export default ProjectsPage;
+const condition = authUser => authUser;
+
+export default withAuthorization(condition)(ProjectsPage);
