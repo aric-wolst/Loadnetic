0db583c0a9e9b7f5281f9f38bae7a455fa14064d
aricw <aric.wolst@gmail.com>
2020-01-28 23:45:55 -0800

    clean repo

--- loadnetic/src/components/Main/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const MainPage = () => (
-    <div>
-        Main
-    </div>
-);
-
-export default MainPage;
--- loadnetic/src/constants/routes.js
+++
@@ -1,7 +0,0 @@
-export const MAIN = '/';
-export const SIGN_UP = '/signup';
-export const SIGN_IN = '/signin';
-export const PROJECTS = '/projects';
-export const ACCOUNT = '/account';
-export const ADMIN = '/admin';
-export const PASSWORD_FORGET = '/pw-forget';
