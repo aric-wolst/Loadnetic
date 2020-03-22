69d7cd9f8abd0777b94062e985e12346bef901b3
aricw <aric.wolst@gmail.com>
2020-03-21 19:19:22 -0700

    added users schema and backend requests

--- backend/server.js
+++ backend/server.js
@@ -3,23 +3,30 @@ const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const mongoose = require('mongoose');
+
+/* Routes */
 const loadneticRoutes = express.Router();
+const userRoutes = express.Router();
 const PORT = 4000;
 
+/* Models */
 let Teams = require('./teams.model');
 let Users = require('./users.model');
 
 app.use(cors());
 app.use(bodyParser.json());
 app.use('/loadnetic', loadneticRoutes);
+app.use('/users', userRoutes);
 
-mongoose.connect('mongodb://127.0.0.1:27017/loadnetic', { useNewUrlParser: true });
+/* Connect to mongo */
+mongoose.connect('mongodb://127.0.0.1:27017', { useNewUrlParser: true });
 const connection = mongoose.connection;
 
 connection.once('open', function() {
     console.log("MongoDB database connection established successfully");
 });
 
+/* Get all organizations */
 loadneticRoutes.route('/').get(function(req, res) {
     Teams.find(function(err, loadnetic) {
         if (err) {
@@ -30,6 +37,7 @@ loadneticRoutes.route('/').get(function(req, res) {
     });
 });
 
+/* Get organization by id */
 loadneticRoutes.route('/:id').get(function(req, res) {
     let id = req.params.id;
     Teams.findById(id, function(err, loadnetic) {
@@ -37,6 +45,7 @@ loadneticRoutes.route('/:id').get(function(req, res) {
     });
 });
 
+/* Add organization */
 loadneticRoutes.route('/add').post(function(req, res) {
     let team = new Teams(req.body);
     team.save()
@@ -48,6 +57,7 @@ loadneticRoutes.route('/add').post(function(req, res) {
         });
 });
 
+/* Update organization */
 loadneticRoutes.route('/update/:id').post(function(req, res) {
     Teams.findById(req.params.id, function(err, team) {
         if (!team) {
@@ -69,7 +79,56 @@ loadneticRoutes.route('/update/:id').post(function(req, res) {
     });
 });
 
-app.use('/loadnetic', loadneticRoutes);
+/* Get all users */
+userRoutes.route('/').get(function(req, res) {
+    Users.find(function(err, users) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.json(users);
+        }
+    });
+});
+
+/* Get user by Id */
+userRoutes.route('/:id').get(function(req, res) {
+    let id = req.params.id;
+    Users.findById(id, function(err, user) {
+        res.json(user);
+    });
+});
+
+/* Add user */
+userRoutes.route('/add').post(function(req, res) {
+    let user = new Users(req.body);
+    user.save()
+        .then(user => {
+            res.status(200).json({'user': 'user added successfully'});
+        })
+        .catch(err => {
+            res.status(400).send('adding new user failed');
+        });
+});
+
+/* Update organization */
+userRoutes.route('/update/:id').post(function(req, res) {
+    Users.findById(req.params.id, function(err, user) {
+        if (!user) {
+            res.status(404).send("data is not found");
+        } else {
+            user.name = req.body.name;
+            user.username = req.body.username;
+            user.password = req.body.password;
+            user.organizations = req.body.organizations;
+
+            user.save().then(user => {
+                res.json('User updated!');
+            }).catch(err => {
+                res.status(400).send("Update not possible");
+            });
+        }
+    });
+});
 
 app.listen(PORT, function() {
     console.log("Server is running on Port: " + PORT);
--- loadnetic/src/components/landing.js
+++ loadnetic/src/components/landing.js
@@ -1,6 +1,21 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 export default class Landing extends Component {
+    constructor(props) {
+        super(props);
+
+        this.userLogin = this.userLogin.bind(this);
+
+        this.state = {
+            user: ''
+        }
+    }
+
+    userLogin () {
+
+    }
+
     render() {
         return (
             <div>
